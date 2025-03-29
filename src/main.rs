0e6cb874b45d851168da0501680fad037c09a761
Ali Firas <ali.albdaer.py@gmail.com>
2025-03-30 01:26:01 +0300

    Basic server on localhost

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, web!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    "欢迎来到 rust web 服务器!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let host: &str = "127.0.0.1";
+    let port: u16 = 8080;
+
+
+    let server = HttpServer::new(|| App::new()
+        .service(home)
+    )
+        .bind((host, port))?
+        .run();
+
+    println!("Server is running on {host}:{port}");
+    server.await
 }
