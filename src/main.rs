ff1d0928fb6d6b54976e6217d51f6c181e2f0a9b
Ali Firas <ali.albdaer.py@gmail.com>
2025-03-30 02:05:07 +0300

    variable welcome message

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
+use std::env;
+use std::io;
 
 #[get("/")]
-async fn root() -> impl Responder {
-    "欢迎来到 rust web 服务器!"
+async fn root(message: web::Data<String>) -> impl Responder {
+    message.get_ref().clone()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let host: &str = "127.0.0.1";
     let port: u16 = 8080;
+    let args: Vec<String> = env::args().collect();
+    
+    
+    let message = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        let mut input = String::new();
+        println!("Enter a message to display on the server: ");
 
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    let server = HttpServer::new(|| App::new()
+        input.trim().to_string()
+    };
+
+    let server: actix_web::dev::Server = HttpServer::new(move || App::new()
+        .app_data(web::Data::new(message.clone()))
         .service(root)
     )
         .bind((host, port))?
